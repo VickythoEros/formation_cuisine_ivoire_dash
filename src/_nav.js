2898f7471d56431f69f4a36fd611e4c82f9ab80e
VickythoEros <vakaramokokone@gmail.com>
2022-11-06 17:13:18 +0000

    first commit

---
+++ src/_nav.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import {
+  cilChartPie,
+  cilDescription,
+  cilNotes,
+  cilSettings,
+  cilPeople,
+  cilPuzzle,
+  cilSpeedometer,
+} from '@coreui/icons'
+import { CNavGroup, CNavItem } from '@coreui/react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavGroup,
+    name: 'Acteurs',
+    to: '/acteurs',
+    icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Administrateurs',
+        to: '/acteurs/administrateurs',
+      },
+      {
+        component: CNavItem,
+        name: 'Mentors',
+        to: '/acteurs/mentors',
+      },
+      {
+        component: CNavItem,
+        name: 'Apprenants',
+        to: '/acteurs/apprenants',
+      },
+    ],
+  },
+
+  {
+    component: CNavItem,
+    name: 'Cours',
+    to: '/cours',
+    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Leçons',
+    to: '/leçons',
+    icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Questions',
+    to: '/questions',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Statistiques',
+    to: '/statistiques',
+    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Configurations',
+    to: '/configurations',
+    icon: <CIcon icon={cilSettings} customClassName="nav-icon" />,
+  },
+ 
+]
+
+export default _nav
---
+++ src/components/AppHeader.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { IconButton, } from 'rsuite';
+import AlignCenterIcon from '@rsuite/icons/legacy/AlignCenter';
+import {
+  CContainer,
+  CHeader,
+  CHeaderBrand,
+  CHeaderNav,
+  CHeaderToggler,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilMenu } from '@coreui/icons'
+
+import { AppHeaderDropdown } from './header/index'
+import { logo } from 'src/assets/brand/logo'
+
+const AppHeader = () => {
+  const dispatch = useDispatch()
+  const sidebarShow = useSelector((state) => state.sidebarShow)
+
+  return (
+    <CHeader position="sticky" className="mb-4">
+      <CContainer fluid>
+        <CHeaderToggler
+          className="ps-1"
+          onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
+        >
+           <IconButton 
+              className=""
+              onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })} 
+              icon={<AlignCenterIcon />} 
+              color="green" 
+              appearance="primary" 
+              circle />
+        </CHeaderToggler>
+        
+     
+
+        <CHeaderBrand className="mx-auto d-md-none" to="/">
+          <CIcon icon={logo} height={48} alt="Logo" />
+        </CHeaderBrand>
+        {/* <CHeaderNav className="d-none d-md-flex me-auto">
+          <CNavItem>
+            <CNavLink to="/dashboard" component={NavLink}>
+              Dashboard
+            </CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href="#">Users</CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href="#">Settings</CNavLink>
+          </CNavItem>
+        </CHeaderNav> */}
+        {/* <CHeaderNav>
+          <CNavItem>
+            <CNavLink href="#">
+              <CIcon icon={cilBell} size="lg" />
+            </CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href="#">
+              <CIcon icon={cilList} size="lg" />
+            </CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href="#">
+              <CIcon icon={cilEnvelopeOpen} size="lg" />
+            </CNavLink>
+          </CNavItem>
+        </CHeaderNav> */}
+        <CHeaderNav className="ms-3">
+          <AppHeaderDropdown />
+        </CHeaderNav>
+      </CContainer>
+    </CHeader>
+  )
+}
+
+export default AppHeader
